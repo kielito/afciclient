a9461c96171159d99354681b776632f7eaac2d74
Ezequiel Esquivel <ezequielesquivel@gmail.com>
2021-08-30 23:53:05 -0300

    Subiendo alta declaracion jurada individual, falta otros integrantes, productos primarios o valor agregado y tipo y productos ofertados. Armar tabla dinamica

--- src/app/components/declaracion/declaracion-abm/declaracion-abm.component.ts
+++ src/app/components/declaracion/declaracion-abm/declaracion-abm.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DeclaracionService } from '../../../services/declaracion.service';
 import { UsuariosService } from '../../../services/usuarios.service';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-declaracion-abm',
@@ -9,12 +9,38 @@ import {Router} from '@angular/router';
   styleUrls: ['./declaracion-abm.component.css']
 })
 export class DeclaracionAbmComponent implements OnInit {
+  //declaracionIndividual = { selloPAFCI: "", selloAFCI: "", fecha_alta: "", fecha_firma_ddjj: "", tipo_dec_jurada: "", pcia_usuario: "", dni_usuario: "" };
+  
+  declaraciones: any = []; //variable del componente, disponible para todas las clases (la puedo usar desde el HTML)
+  confirmacion: boolean = false;
+  error: boolean = false;
+  mensaje = "";
+  usuario: any = [];  
 
-  constructor(private declaracionService:DeclaracionService, private router:Router, private usuariosService:UsuariosService) { }
+  constructor(private declaracionService: DeclaracionService, private router: Router, private usuariosService: UsuariosService) { }
 
   ngOnInit(): void {
     this.usuariosService.logued$.emit();
-    
+    this.usuario = JSON.parse(localStorage.usuario);
+    this.confirmacion = false;
+    this.error = false;
+  }
+
+  registrar() {
+    this.confirmacion = false;
+    this.error = false;
+    this.usuario.tipo_dec_jurada = "I";
+    this.declaracionService.agregarDeclaracion(this.usuario).subscribe(
+      res => {
+        this.ngOnInit();
+        this.mensaje = "agregada";
+        this.confirmacion = true;
+      },
+      err => {
+        this.error = true;
+        this.mensaje = err.error.message;
+      }
+    )
   }
 
 }
--- src/app/services/declaracion.service.ts
+++ src/app/services/declaracion.service.ts
@@ -1,30 +1,33 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class DeclaracionService {
-  //API_URI = 'http://localhost:3000/user'; //variable local a la clase con la ruta
-  //API_URI = 'https://afcisellosserver.herokuapp.com/user';
-  constructor(private http: HttpClient, private router: Router) { }
-  //PROVEEDORES	
-  /*
-	listarProveedor(){ //METODO		
-		return this.http.get(`${this.API_URI}/listar`); //http: variable definida en el constructor / Parametro se pasa la ruta principal seguida del metodo q quiero utilizar		
-	}
-	
-	agregarProveedor(proveedor:any){ //METODO
-		return this.http.post(`${this.API_URI}/agregar`, proveedor);
-	}
+	//API_URI = 'http://localhost:3000/declaracion'; //variable local a la clase con la ruta
+	API_URI = 'https://afcisellosserver.herokuapp.com/declaracion';
+	//logued$: EventEmitter<string> = new EventEmitter<string>();
+	constructor(private http: HttpClient, private router: Router) { }
 
-	editarProveedor(proveedor:any){ //METODO
-		return this.http.post(`${this.API_URI}/editar`, proveedor);
+	agregarDeclaracion(usuario: any) { //METODO
+		return this.http.post(`${this.API_URI}/add`, usuario);
 	}
-
-	eliminarProveedor(id:string){
-		return this.http.delete(`${this.API_URI}/eliminar/${id}`);
-	}
-  */
+	//PROVEEDORES	
+	/*
+	  listarProveedor(){ //METODO		
+		  return this.http.get(`${this.API_URI}/listar`); //http: variable definida en el constructor / Parametro se pasa la ruta principal seguida del metodo q quiero utilizar		
+	  }
+  	
+  	
+  
+	  editarProveedor(proveedor:any){ //METODO
+		  return this.http.post(`${this.API_URI}/editar`, proveedor);
+	  }
+  
+	  eliminarProveedor(id:string){
+		  return this.http.delete(`${this.API_URI}/eliminar/${id}`);
+	  }
+	*/
 }
